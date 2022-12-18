02da63d3a4c5e11c8f70c88e964c2fa77b690c82
nettronco <nettronco@gmail.com>
2022-12-18 04:12:07 +0300

    init part2

---
+++ part_2/tasks.py
@@ -0,0 +1,37 @@
+import turtle
+import time
+
+print('Прямоугольник')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(50)
+
+
+print('Шестиугольник')
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(50)
+turtle.right(60)
+
+
+print('Треугольник равносторонний')
+turtle.left(90)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+
+time.sleep(3)
