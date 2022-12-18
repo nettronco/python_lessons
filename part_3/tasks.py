467726cab9ad32d1b7fd448f1b471571d149b81c
nettronco <nettronco@gmail.com>
2022-12-18 04:47:15 +0300

    add part of part3

---
+++ part_3/tasks.py
@@ -0,0 +1,21 @@
+print('Task 1')
+print('Три числа:', 10, 5, 24)
+print("")
+
+print('Task 2')
+print('Три числа: ', end='')
+print( 103, 25, 74, sep=', ')
+print('')
+
+print('Task 3')
+print( 1, 2, 3, sep='\n\n')
+print('')
+
+print('Task 4')
+import math
+print('% 4.3f '% math.pi, end='\n')
+print('')
+
+print('Task 5')
+
+print('')
